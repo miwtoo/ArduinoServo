235db69cc06c4e6beecebc8854faa074bd89f558
Miwtoo <miwyugioh@gmail.com>
2018-12-28 22:30:48 +0700

    init

---
+++ Android App/servo180/app/src/main/java/servo/project/miwtoo/servo180/MainActivity.java
@@ -0,0 +1,13 @@
+package servo.project.miwtoo.servo180;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
