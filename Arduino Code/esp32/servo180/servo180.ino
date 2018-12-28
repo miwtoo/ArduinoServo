4423f84ad2813f98354f6ad7f4fbcc8bd6bc8f2f
Miwtoo <miwyugioh@gmail.com>
2018-12-28 21:56:01 +0700

    init

---
+++ Arduino Code/esp32/servo180/servo180.ino
@@ -0,0 +1,48 @@
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+int LED = 23;
+String inString = "";
+int pos = 0;
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  //  digitalWrite(LED, HIGH);
+  //  delay(500);
+  //  digitalWrite(LED, LOW);
+  //  delay(500);
+
+  if (SerialBT.available()) {
+    int inChar = SerialBT.read();
+    if (isDigit(inChar)) {
+      // convert the incoming byte to a char and add it to the string:
+      inString += (char)inChar;
+    }
+    // if you get a newline, print the string, then the string's value:
+    if (inChar == '\n') {
+      pos = inString.toInt();
+      Serial.println(pos);
+
+      if(pos == 69){
+        digitalWrite(LED,HIGH);  
+      }
+      else if(pos == 98){
+        digitalWrite(LED,LOW);  
+        }
+      
+      inString = "";
+    }
+  }
+}
